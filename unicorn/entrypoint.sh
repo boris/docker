35826dd9db8dec58c8838633a79a02ef555faedd
Boris Quiroz <boris@insert-coin.org>
2017-05-07 01:13:16 -0300

    precompile assets

--- unicorn/entrypoint.sh
+++ unicorn/entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 APP_ROOT='/home/deploy/app'
 cd $APP_ROOT
 bundle install --path vendor/bundle
-cd ..
+RAILS_ENV=production bundle exec rake assets:precompile
 RAILS_ENV=production unicorn -c /home/deploy/app/config/unicorn.rb
