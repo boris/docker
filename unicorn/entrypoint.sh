9132293fd25f7638826056a01c6945fc51f90a5d
Boris Quiroz <boris@insert-coin.org>
2017-08-09 00:12:39 -0400

    fix for migration

--- unicorn/entrypoint.sh
+++ unicorn/entrypoint.sh
@@ -5,4 +5,5 @@ APP_ROOT='/home/deploy/app'
 cd $APP_ROOT
 bundle install --path vendor/bundle
 RAILS_ENV=production bundle exec rake assets:precompile
+RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production unicorn -c /home/deploy/app/config/unicorn.rb
