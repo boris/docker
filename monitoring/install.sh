11d1019fdc9c44a9481085f42b0f0268af04699c
Boris Quiroz <boris@insert-coin.org>
2017-08-22 16:50:38 -0300

    fix install script

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-mkdir -p $(pwd)/monitoring/prometheus
-curl -O $(pwd)/monitoring/prometheus/prometheus.yml https://raw.githubusercontent.com/boris/docker/master/monitoring/prometheus/prometheus.yml
-curl -O $(pwd)/monitoring https://raw.githubusercontent.com/boris/docker/master/monitoring/monitoring.yml
+main() {
+  mkdir -p $(pwd)/monitoring/prometheus
+  cd $(pwd)/monitoring/prometheus
+  curl -sO https://raw.githubusercontent.com/boris/docker/master/monitoring/prometheus/prometheus.yml
+  cd ..
+  curl -sO  https://raw.githubusercontent.com/boris/docker/master/monitoring/monitoring.yml
+
+}
+main
+
