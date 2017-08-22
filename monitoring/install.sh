d41068808cbec6448e97cc273f38f68bd5027f23
Boris Quiroz <boris@insert-coin.org>
2017-08-22 16:39:33 -0300

    deploy script

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p $(pwd)/monitoring/prometheus
-curl -O $(pwd)/monitoring/prometheus/prometheus.yml 
-curl -O $(pwd)/monitoring 
+curl -O $(pwd)/monitoring/prometheus/prometheus.yml https://raw.githubusercontent.com/boris/docker/master/monitoring/prometheus/prometheus.yml
+curl -O $(pwd)/monitoring https://raw.githubusercontent.com/boris/docker/master/monitoring/monitoring.yml
